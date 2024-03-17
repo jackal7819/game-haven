3fbe42a6895e69018df5039b6250ac34a9fa88d3
Viktor Filippov <jackal3166@gmail.com>
2024-03-17 22:19:12 +0200

    Add star rating

--- components/Main/CardGame.tsx
+++ components/Main/CardGame.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Image from 'next/image';
-import { CiHeart, CiShoppingCart } from 'react-icons/ci';
+import { CiHeart, CiShoppingCart, CiStar } from 'react-icons/ci';
+import { FaRegStar, FaStar } from 'react-icons/fa';
 
 interface Game {
 	id: number;
@@ -28,13 +31,25 @@ const CardGame: React.FC<CardGameProps> = ({ game }) => {
 			</div>
 			<div className='flex flex-col justify-between flex-1 gap-2 p-4'>
 				<div className='flex items-center justify-between w-full'>
-					<p>{game.date}</p>
+					{/* <p>{game.date}</p> */}
+					<div className='flex gap-1'>
+						{Array.from({ length: 5 }, (_, i) =>
+							game.rating > i ? (
+								<FaStar size={19} className='fill-[#DB5B32]' />
+							) : (
+								<FaRegStar
+									size={19}
+									className='fill-[#DB5B32]'
+								/>
+							)
+						)}
+					</div>
 					<p className='text-2xl'>{game.price} €</p>
 				</div>
 				<div className='flex items-center justify-between w-full'>
 					<h2 className='flex-1 text-2xl'>{game.title}</h2>
 					<div className='flex items-center justify-center p-3'>
-					<CiHeart size={27} />
+						<CiHeart size={27} />
 					</div>
 					<div className='flex items-center justify-center p-3 bg-[#DB5B32] rounded-full'>
 						<CiShoppingCart size={18} />
