119dc3205eb937387b9ce28ae9ff4ae899bc4b8f
Viktor Filippov <jackal3166@gmail.com>
2024-03-17 20:26:56 +0200

    Add styles to CardGame

--- components/Main/CardGame.tsx
+++ components/Main/CardGame.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import { CiHeart, CiShoppingCart } from 'react-icons/ci';
+
 interface Game {
 	id: number;
 	title: string;
@@ -13,8 +16,31 @@ interface CardGameProps {
 
 const CardGame: React.FC<CardGameProps> = ({ game }) => {
 	return (
-		<div className='max-w-[514px] h-[393px] border border-white'>
-			{game.title}
+		<div className='max-w-[514px] h-[393px] flex flex-col gap-2 hover:shadow-lg hover:shadow-slate-700 rounded-xl duration-300 hover:-translate-y-3 cursor-pointer'>
+			<div className='rounded-xl'>
+				<Image
+					className='rounded-xl'
+					src={game.image}
+					alt={game.title}
+					width={514}
+					height={261}
+				/>
+			</div>
+			<div className='flex flex-col justify-between flex-1 gap-2 p-4'>
+				<div className='flex items-center justify-between w-full'>
+					<p>{game.date}</p>
+					<p className='text-2xl'>{game.price} €</p>
+				</div>
+				<div className='flex items-center justify-between w-full'>
+					<h2 className='flex-1 text-2xl'>{game.title}</h2>
+					<div className='flex items-center justify-center p-3'>
+					<CiHeart size={27} />
+					</div>
+					<div className='flex items-center justify-center p-3 bg-[#DB5B32] rounded-full'>
+						<CiShoppingCart size={18} />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
