1fddff698d8430bf94ce12ba0f7c5934629f2487
Ivan Palii <vanopaliy@gmail.com>
2024-03-20 05:43:55 +0900

    Completed the "Advantages" block

--- components/Main/Icons.tsx
+++ components/Main/Icons.tsx
@@ -1,5 +1,14 @@
+import { iconsData } from "@/iconsData";
+import IconItem from "@/components/Main/IconItem";
+
 const Icons = () => {
-	return <div>Icons</div>;
+  return (
+    <div className="mx-auto mb-20 max-w-[1920px] px-40 max-[1440px]:px-28 max-xl:px-24 max-lg:px-20 max-md:px-10 max-[360px]:px-4 flex gap-x-6 max-sm:flex-col">
+      {iconsData.map((item, index) => (
+        <IconItem key={index} item={item} />
+      ))}
+    </div>
+  );
 };
 
 export default Icons;
