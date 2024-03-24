fe6484d4d4ea9edc971bef04a373763ae1373566
katerynalanina <lanina.kateryna@gmail.com>
2024-03-24 15:17:51 +0200

    feat: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,8 @@ const config: Config = {
         'lg': '1024px',
         'xl': '1280px',
         '2xl': '1440px',
+        '3xl': '1680px',
+        '4xl': '1920px',
       },
       extend: {
         backgroundImage: {
