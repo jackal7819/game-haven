4df68da986caad8d6bc92174f6a0a0c230dad83a
Viktor Filippov <jackal3166@gmail.com>
2024-04-08 23:29:53 +0300

    Create ChangePassword Page

--- app/change-password/page.tsx
+++ app/change-password/page.tsx
@@ -1,40 +1,32 @@
 'use client';
 
-import Link from 'next/link';
-import {
-	BsApple,
-	BsCheck,
-	BsDiscord,
-	BsFacebook,
-	BsFillEyeFill,
-	BsFillEyeSlashFill,
-	BsGoogle,
-} from 'react-icons/bs';
+import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useState } from 'react';
 
 type Inputs = {
-	email: string;
-	emailRequired: string;
 	password: string;
 	passwordRequired: string;
+	confirmPassword: string;
+	confirmPasswordRequired: string;
 };
 
 const ChangePassword: React.FC = () => {
-	const [rememberMe, setRememberMe] = useState(false);
 	const [passwordVisible, setPasswordVisible] = useState(false);
-
-	const handleCheckboxChange = () => {
-		setRememberMe(!rememberMe);
-	};
+	const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
 	const togglePasswordVisibility = () => {
 		setPasswordVisible(!passwordVisible);
 	};
 
+	const toggleConfirmPasswordVisibility = () => {
+		setConfirmPasswordVisible(!confirmPasswordVisible);
+	};
+
 	const {
 		register,
 		handleSubmit,
+		watch,
 		formState: { errors },
 	} = useForm<Inputs>();
 	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
@@ -42,56 +34,15 @@ const ChangePassword: React.FC = () => {
 	return (
 		<div className='p-40 m-auto'>
 			<div className='flex flex-col gap-14 w-[640px]'>
-				<div className='flex flex-col gap-8'>
-					<h1 className='xl:text-5xl text-[32px] font-bold font-orbitron text-center'>
+				<h1 className='xl:text-5xl text-[32px] font-bold font-orbitron text-center'>
 					Change your password
-					</h1>
-					<div className='flex items-center justify-between gap-6'>
-						<BsFacebook
-							size={56}
-							className='duration-500 cursor-pointer hover:text-orange'
-						/>
-						<BsDiscord
-							size={56}
-							className='duration-500 cursor-pointer hover:text-orange'
-						/>
-						<BsGoogle
-							size={56}
-							className='duration-500 cursor-pointer hover:text-orange'
-						/>
-						<BsApple
-							size={56}
-							className='duration-500 cursor-pointer hover:text-orange'
-						/>
-					</div>
-					<p className='text-[32px] font-medium text-center'>or</p>
-				</div>
+				</h1>
 				<form
 					onSubmit={handleSubmit(onSubmit)}
 					className='flex flex-col gap-3'
 				>
-					<label htmlFor='email'>Email</label>
-					<input
-						className='p-2 font-medium duration-500 border-4 rounded-lg outline-none bg-light text-light-gray border-light focus:border-blue'
-						placeholder='Enter your email'
-						type='email'
-						{...register('email', {
-							required: 'Email is required',
-							pattern: {
-								value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
-								message: 'Invalid email address',
-							},
-						})}
-					/>
-					<div className='h-5'>
-						{errors.email && (
-							<span className='text-red-500 '>
-								{errors.email.message}
-							</span>
-						)}
-					</div>
 					<div className='relative flex flex-col gap-3'>
-						<label htmlFor='password'>Password</label>
+						<label htmlFor='password'>New password</label>
 						<input
 							className='p-2 font-medium duration-500 border-4 rounded-lg outline-none bg-light text-light-gray border-light focus:border-blue'
 							placeholder='Enter your password'
@@ -124,16 +75,42 @@ const ChangePassword: React.FC = () => {
 							)}
 						</div>
 					</div>
-					<div className='flex items-center gap-3'>
-						<div
-							className='flex items-center justify-center duration-500 border border-white rounded-sm cursor-pointer size-5'
-							onClick={handleCheckboxChange}
+					<div className='relative flex flex-col gap-3'>
+						<label htmlFor='confirmPassword'>Confirm password</label>
+						<input
+							className='p-2 font-medium duration-500 border-4 rounded-lg outline-none bg-light text-light-gray border-light focus:border-blue'
+							placeholder='Confirm your password'
+							type={confirmPasswordVisible ? 'text' : 'password'}
+							{...register('confirmPassword', {
+								required: 'Password confirmation required',
+								minLength: {
+									value: 8,
+									message:
+										'Password must be at least 8 characters',
+								},
+								validate: (value) =>
+									value === watch('password') ||
+									'Passwords do not match',
+							})}
+						/>
+						<button
+							type='button'
+							className='absolute text-light-gray right-3 top-12'
+							onClick={toggleConfirmPasswordVisibility}
 						>
-							{rememberMe && (
-								<BsCheck size={24} className='text-light' />
+							{confirmPasswordVisible ? (
+								<BsFillEyeSlashFill size={24} />
+							) : (
+								<BsFillEyeFill size={24} />
+							)}
+						</button>
+						<div className='h-5'>
+							{errors.confirmPassword && (
+								<span className='text-red-500 '>
+									{errors.confirmPassword.message}
+								</span>
 							)}
 						</div>
-						<span className='text-light'>Remember me</span>
 					</div>
 					<button
 						type='submit'
@@ -141,20 +118,6 @@ const ChangePassword: React.FC = () => {
 					>
 						Log in
 					</button>
-					<div className='flex justify-between mt-2'>
-						<Link
-							href='/register'
-							className='px-6 text-xl duration-500 text-light hover:text-orange'
-						>
-							No account yet?
-						</Link>
-						<Link
-							href='/reset-password'
-							className='px-6 text-xl duration-500 text-light hover:text-orange'
-						>
-							Forgot password
-						</Link>
-					</div>
 				</form>
 			</div>
 		</div>
