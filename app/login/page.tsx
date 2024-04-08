bd2d0776ff249e5e84de4c6b5657957c77e7ac60
Viktor Filippov <jackal3166@gmail.com>
2024-04-08 22:33:18 +0300

    Create Login Page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,17 @@
 'use client';
 
-import { BsApple, BsDiscord, BsFacebook, BsGoogle } from 'react-icons/bs';
+import Link from 'next/link';
+import {
+	BsApple,
+	BsCheck,
+	BsDiscord,
+	BsFacebook,
+	BsFillEyeFill,
+	BsFillEyeSlashFill,
+	BsGoogle,
+} from 'react-icons/bs';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 type Inputs = {
 	email: string;
@@ -11,6 +21,17 @@ type Inputs = {
 };
 
 const Login: React.FC = () => {
+	const [rememberMe, setRememberMe] = useState(false);
+	const [passwordVisible, setPasswordVisible] = useState(false);
+
+	const handleCheckboxChange = () => {
+		setRememberMe(!rememberMe);
+	};
+
+	const togglePasswordVisibility = () => {
+		setPasswordVisible(!passwordVisible);
+	};
+
 	const {
 		register,
 		handleSubmit,
@@ -19,7 +40,7 @@ const Login: React.FC = () => {
 	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
 	return (
-		<div className='m-auto'>
+		<div className='p-40 m-auto'>
 			<div className='flex flex-col gap-14 w-[640px]'>
 				<div className='flex flex-col gap-8'>
 					<h1 className='xl:text-5xl text-[32px] font-bold font-orbitron text-center'>
@@ -51,6 +72,8 @@ const Login: React.FC = () => {
 				>
 					<label htmlFor='email'>Email</label>
 					<input
+						className='p-2 font-medium duration-500 border-4 rounded-lg outline-none bg-light text-light-gray border-light focus:border-blue'
+						placeholder='Enter your email'
 						type='email'
 						{...register('email', {
 							required: 'Email is required',
@@ -67,10 +90,12 @@ const Login: React.FC = () => {
 							</span>
 						)}
 					</div>
-
-					<label htmlFor='password'>Password</label>
+					<div className='relative flex flex-col gap-3'>
+						<label htmlFor='password'>Password</label>
 						<input
-							type='password'
+							className='p-2 font-medium duration-500 border-4 rounded-lg outline-none bg-light text-light-gray border-light focus:border-blue'
+							placeholder='Enter your password'
+							type={passwordVisible ? 'text' : 'password'}
 							{...register('password', {
 								required: 'Password is required',
 								minLength: {
@@ -80,14 +105,56 @@ const Login: React.FC = () => {
 								},
 							})}
 						/>
-					<div className='h-5'>
-						{errors.password && (
-							<span className='text-red-500 '>
-								{errors.password.message}
-							</span>
-						)}
+						<button
+							type='button'
+							className='absolute text-light-gray right-3 top-12'
+							onClick={togglePasswordVisibility}
+						>
+							{passwordVisible ? (
+								<BsFillEyeSlashFill size={24} />
+							) : (
+								<BsFillEyeFill size={24} />
+							)}
+						</button>
+						<div className='h-5'>
+							{errors.password && (
+								<span className='text-red-500 '>
+									{errors.password.message}
+								</span>
+							)}
+						</div>
 					</div>
-					<button type='submit'>Log in</button>
+					<div className='flex items-center gap-3'>
+						<div
+							className='flex items-center justify-center duration-500 border border-white rounded-sm cursor-pointer size-5'
+							onClick={handleCheckboxChange}
+						>
+							{rememberMe && (
+								<BsCheck size={24} className='text-light' />
+							)}
+						</div>
+						<span className='text-light'>Remember me</span>
+					</div>
+					<button
+						type='submit'
+						className='p-4 text-2xl duration-500 rounded-2xl bg-orange text-light mt-9 hover:opacity-80 active:opacity-60'
+					>
+						Log in
+					</button>
+					<div className='flex justify-between mt-2'>
+						<Link
+							href='/register'
+							className='px-6 text-xl duration-500 text-light hover:text-orange'
+						>
+							No account yet?
+						</Link>
+						<Link
+							href='/reset-password'
+							className='px-6 text-xl duration-500 text-light hover:text-orange'
+						>
+							Forgot password
+						</Link>
+					</div>
 				</form>
 			</div>
 		</div>
