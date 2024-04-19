03c52a28fa4e4301d9f7a853c7f46331733e920a
Ivan Palii <vanopaliy@gmail.com>
2024-04-19 23:17:00 +0900

    Created a News page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,23 +3,32 @@ import Pagination from '@/components/News/Pagination';
 import Sales from '@/components/News/Sales';
 import Upcoming from '@/components/News/Upcoming';
 
+import {newsData} from "@/newsData";
+
 const News = () => {
 	return (
 		<div className='container'>
-			<h1 className='xl:text-5xl text-[32px] font-bold font-orbitron mb-14'>
+			<h1 className='xl:text-5xl text-[32px] font-bold font-orbitron mb-8 md:mb-14'>
 				Latest news
 			</h1>
-			<New />
-			<New />
-			<New />
-			<Upcoming />
-			<New />
-			<New />
-			<New />
-			<Sales />
-			<New />
-			<New />
-			<New />
+
+			{newsData.map((value, index) => {
+				if (index === 2) {
+					return (
+						<>
+							<New key={value.id} item={value}/>
+							<Upcoming />
+						</>
+					);
+				} else if (index === 5) {
+					return (
+						<>
+							<New key={value.id} item={value}/>
+							<Sales />
+						</>
+					);
+				} else return <New key={value.id} item={value}/>;
+			})}
 			<Pagination />
 		</div>
 	);
