1fee3242d670e9b75ec3f8b48e6f2d4e6f8ee76e
jackal7819 <jackal3166@gmail.com>
2024-04-26 15:28:01 +0300

    Add SeeMore and SeeMoreMobile

--- components/Main/NewReleases.tsx
+++ components/Main/NewReleases.tsx
@@ -1,8 +1,8 @@
 'use client';
-import Link from 'next/link';
-import { GoChevronRight } from 'react-icons/go';
 import { games } from '@/data';
 import CardGame from './CardGame';
+import SeeMore from '../Layout/Button/SeeMore';
+import SeeMoreMobile from '../Layout/Button/SeeMoreMobile';
 
 const NewReleases: React.FC = () => {
 	const innerWidth = window.innerWidth;
@@ -15,25 +15,9 @@ const NewReleases: React.FC = () => {
 					New Releases
 				</h2>
 				{/* DESKTOP LINK */}
-				<Link
-					href='/'
-					className='items-center justify-center hidden gap-2 px-4 py-4 text-transparent duration-300 border rounded-lg md:flex border-b-light-blue w-44 h-14 border-r-blue border-t-blue border-l-light-blue hover:border-r-light-blue hover:border-b-blue hover:border-l-blue hover:border-t-light-blue group'
-				>
-					<span className='text-xl bg-gradient-to-r from-blue to-light-blue bg-clip-text group-hover:bg-gradient-to-l'>
-						See more
-					</span>
-					<GoChevronRight
-						size={24}
-						className='mt-[3px] text-blue group-hover:text-light-blue'
-					/>
-				</Link>
+				<SeeMore />
 				{/* MOBILE LINK */}
-				<Link href='/' className='md:hidden'>
-					<GoChevronRight
-						size={24}
-						className='mt-[3px] text-blue hover:text-light-blue'
-					/>
-				</Link>
+				<SeeMoreMobile />
 			</div>
 			{/* DESKTOP CARDS */}
 			<div className='hidden grid-cols-1 gap-4 md:grid md:grid-cols-2 xl:grid-cols-3 4xl:gap-7 xl:gap-5 2xl:gap-6 4xl:grid-cols-4'>
