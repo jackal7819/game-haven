fe7481cf98d482f1ee9f9355d5851698c8fa34d8
jackal7819 <jackal3166@gmail.com>
2024-04-26 15:49:51 +0300

    Add page Catalog

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,7 +1,18 @@
+import Filters from '@/components/Catalog/Filters';
+import GamesCatalog from '@/components/Catalog/GamesCatalog';
+import Pagination from '@/components/News/Pagination';
+
 const Catalog = () => {
 	return (
-		<div>
+		<div className='container'>
 			<h1>Catalog</h1>
+			<div className='flex gap-3'>
+				<Filters />
+				<div>
+					<GamesCatalog />
+					<Pagination />
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ components/Catalog/GamesCatalog.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GamesCatalog = () => {
+	return (
+		<div>
+			<h1>Games Catalog</h1>
+		</div>
+	);
+};
+
+export default GamesCatalog;
